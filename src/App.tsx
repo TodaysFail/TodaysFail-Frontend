78712cb2b88b693992c23fea23960a27a65803c1
swywssaid <swywssaid@gmail.com>
2023-07-27 20:37:20 +0900

    chore: 스타일 세팅
    - reset css
    - themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import './App.css';
+import * as React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import reset from 'styled-reset';
+import theme from './styles/theme';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  /* other styles */
+`;
 
 function App() {
-  return <div>app</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <React.Fragment>
+        <GlobalStyle />
+        <div>app</div>
+      </React.Fragment>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+const theme = {
+  colors: {
+    primary: '',
+    secondary: '',
+  },
+};
+
+export default theme;
