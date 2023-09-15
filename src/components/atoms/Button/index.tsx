1cd6958446a786df405f70bb82f4ed189bcd0d37
Seongsu <seongsu@gimseongsuui-MacBookAir-2.local>
2023-09-15 17:57:56 +0900

    [#3] feat: 버튼 공통 컴포넌트 아이콘 추가
    close #3

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,4 +1,10 @@
 import styled from 'styled-components';
+import { ReactComponent as PlusEnabledIcon } from '@assets/plus-enabled.svg';
+import { ReactComponent as PlusHoverIcon } from '@assets/plus-hover.svg';
+import { ReactComponent as PlusDisabledIcon } from '@assets/plus-disabled.svg';
+import { ReactComponent as PlusFilledIcon } from '@assets/plus-filled.svg';
+import { useState } from 'react';
+import { StyleSheetManager } from 'styled-components';
 
 interface ButtonProps {
   type?: 'outlined' | 'filled';
@@ -6,18 +12,28 @@ interface ButtonProps {
   disabled?: boolean;
   onClick?: () => void;
   icon?: boolean;
-  children: React.ReactNode;
+  children?: React.ReactNode;
 }
 
-const StyledButton = styled.button<ButtonProps>`
+interface StyledButtonProps {
+  type?: 'outlined' | 'filled';
+  size?: 'small' | 'large' | 'long';
+  disabled?: boolean;
+  onClick?: () => void;
+  icon?: boolean;
+  children?: React.ReactNode;
+}
+
+const StyledButton = styled.button<StyledButtonProps>`
   /* 공통 스타일 */
   display: inline-flex;
   justify-content: center;
   align-items: center;
   gap: 0.25rem;
   flex-shrink: 0;
-  height: 2.75rem;
   border-radius: 0.5rem;
+  font-family: 'SpoqaHanSansNeo-Regular';
+  font-size: 0.875rem;
 
   /* 버튼 타입에 따른 스타일 */
   background-color: ${(props) =>
@@ -30,13 +46,13 @@ const StyledButton = styled.button<ButtonProps>`
     props.type === 'filled'
       ? 'var(--white)'
       : props.disabled
-      ? 'var(--button-disabled)'
+      ? 'var(--gray-400)'
       : 'var(--primary)'};
   border: ${(props) =>
     props.type === 'filled'
       ? 'transparent'
       : props.disabled
-      ? '1px solid var(--button-disabled)'
+      ? '1px solid var(--gray-400)'
       : '1px solid var(--primary)'};
 
   /* 마우스 호버 시 스타일 */
@@ -51,13 +67,26 @@ const StyledButton = styled.button<ButtonProps>`
       props.type === 'filled'
         ? 'var(--white)'
         : props.disabled
-        ? 'var(--button-disabled)'
+        ? 'var(--gray-400)'
         : 'var(--orange-500)'};
   }
 
   /* 버튼 사이즈에 따른 스타일 */
   min-width: ${(props) => {
     if (props.size === 'long') return '20.5rem';
+  }};
+
+  height: ${(props) => {
+    switch (props.size) {
+      case 'small':
+        return '2rem 2rem';
+      case 'large':
+        return '2.75rem';
+      case 'long':
+        return '2.75rem';
+      default:
+        return '2rem';
+    }
   }};
   padding: ${(props) => {
     switch (props.size) {
@@ -74,11 +103,6 @@ const StyledButton = styled.button<ButtonProps>`
 
   /* 비활성화 시 스타일링 */
   cursor: ${(props) => (props.disabled ? 'default' : 'pointer')};
-
-  /* 아이콘만 있을 때 스타일 */
-  padding: ${(props) => {
-    if (!props.children) return '0.5rem';
-  }};
 `;
 
 function Button({
@@ -89,17 +113,48 @@ function Button({
   icon = false,
   children,
 }: ButtonProps) {
-  console.log(type, size, icon);
+  const [isPlusIconActive, setIsPlusIconActive] = useState(false);
+  console.log(type, size, icon, isPlusIconActive, !children);
+  const handlePlusIconActive = () => {
+    setIsPlusIconActive((prev) => !prev);
+  };
+
   return (
-    <StyledButton
-      type={type}
-      size={size}
-      disabled={disabled}
-      onClick={disabled ? undefined : onClick}
-      icon={icon}
-    >
-      {children}
-    </StyledButton>
+    <StyleSheetManager shouldForwardProp={(prop) => prop !== 'icon'}>
+      <StyledButton
+        type={type}
+        size={size}
+        disabled={disabled}
+        onClick={disabled ? undefined : onClick}
+        icon={icon}
+        onMouseEnter={handlePlusIconActive}
+        onMouseLeave={handlePlusIconActive}
+      >
+        {icon &&
+          size !== 'long' &&
+          (type === 'filled' ? (
+            <PlusFilledIcon />
+          ) : disabled ? (
+            <PlusDisabledIcon />
+          ) : isPlusIconActive ? (
+            <PlusHoverIcon />
+          ) : (
+            <PlusEnabledIcon />
+          ))}
+        {children}
+        {icon &&
+          size === 'long' &&
+          (type === 'filled' ? (
+            <PlusFilledIcon />
+          ) : disabled ? (
+            <PlusDisabledIcon />
+          ) : isPlusIconActive ? (
+            <PlusHoverIcon />
+          ) : (
+            <PlusEnabledIcon />
+          ))}
+      </StyledButton>
+    </StyleSheetManager>
   );
 }
 
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,12 +1,3 @@
-import Button from '@components/atoms/Button';
-
 export default function Home() {
-  const handleClick = () => {
-    console.log('클릭');
-  };
-  return (
-    <div>
-      <Button onClick={handleClick}>버튼</Button>
-    </div>
-  );
+  return <div></div>;
 }
